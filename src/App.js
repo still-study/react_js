218f1989e958a1139da4a26d6725d3f22876f9d2
Alexey <xpost87@mail.ru>
2022-01-14 18:45:41 +0500

    Lesson 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import {Message} from './components/Message';
 
-function App() {
+const myText = 'Props TEXT';
+
+export function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        My first react App
+        <h3>Hello, {props.name}!</h3>
+        <Message text = {myText}/>
       </header>
     </div>
   );
 }
-
-export default App;
